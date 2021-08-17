3f453f259f0fe19f4955d271ca18db684837a66c
nikita <nik.cet@mail.ru>
2021-08-17 21:34:59 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+function App() {
+  return (
+    <div className="container">
+        <h1>Hello World</h1>
+        <p>Четвериков Никита Сергеевич</p>
+    </div>
+  );
+}
+
+export default App;
